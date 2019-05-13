5c93fdcbfa1f80469e520a25e629c41c9ed7f726
Ashley Wilkes <janonderek@gmail.com>
2019-05-13 02:28:44 +0200

    Fixup. BoxSelector interface

--- src/BoxSelector.hpp
+++ src/BoxSelector.hpp
@@ -1,5 +1,9 @@
+#pragma once
 #include "boost/range/iterator_range_core.hpp"
+#include <map>
+#include <memory>
 #include <string>
+#include <vector>
 //Abstract class. Provides template method select, which clients may
 //use to select an element from a range.
 //
@@ -11,18 +15,24 @@ namespace ashley::progress_tracker {
     public:
       using BS_ID = std::string;
       using BS_Iter = std::vector<std::string>::iterator;
-      using IntIter = std::vector<int>::iterator;
+      using BS_UPtr = std::unique_ptr<BoxSelector>;
+      using BS_Range = boost::iterator_range<BS_Iter>;
 
-      static boost::iterator_range<BS_Iter> getKnownBsIds();
+      using IntIter = std::vector<int>::iterator;
+      using IntRange = boost::iterator_range<IntIter>;
+
+      static BS_Range getKnownBsIds();
 
       //static int select(IntIter first, IntIter last, BS_ID nbsId);
-      static std::size_t select( boost::iterator_range<IntIter> inputRange, BS_ID nbsId );
+      static std::size_t select( IntRange inputRange, BS_ID nbsId );
 
-      static std::unique_ptr<BoxSelector> create( BS_ID nbsId );
+      static BS_UPtr& create( BS_ID nbsId );
 
       //virtual int select( IntIter first, IntIter last ) const = 0;
-      virtual std::size_t select( boost::iterator_range<IntIter> inputRange ) const = 0;
+      virtual std::size_t select( IntRange inputRange ) const = 0;
 
       virtual ~BoxSelector() = 0;
+    private:
+      static std::map<BS_ID, BS_UPtr> selectorsMap_;
   };
 }
