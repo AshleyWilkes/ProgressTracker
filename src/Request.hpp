50852c83ee8d31fef20b135eaa26a798b95e1680
Ashley Wilkes <janonderek@gmail.com>
2019-05-15 06:49:20 +0200

    Fixup. Better Request interface.

--- src/Request.hpp
+++ src/Request.hpp
@@ -7,17 +7,22 @@ namespace ashley::progress_tracker {
       using UPS = std::unique_ptr<PS>;
 
       Request( std::size_t totalCapacity );
+      UPS useCapacity( std::size_t capacity );
 
-      virtual UPS useCapacity( std::size_t capacity ) = 0;
-
-      virtual bool isFullyProcessed() const = 0;
+      bool isFullyProcessed() const;
+      std::size_t getTotalCapacity() const;
+      std::size_t getUsedCapacity() const;
+      std::size_t getAvailableCapacity() const;
 
       virtual ~Request() = 0;
     private:
+      virtual UPS createProgressStep( std::size_t capacity ) = 0;
+
       const std::size_t totalCapacity_;
+      const std::size_t usedCapacity_;
   };
 
-  template<typename PS> Request<PS>::Request( std::size_t totalCapacity ) : totalCapacity_{ totalCapacity } {
+  template<typename PS> Request<PS>::Request( std::size_t totalCapacity ) : totalCapacity_{ totalCapacity }, usedCapacity_{ 0 } {
     if ( totalCapacity < 1 ) {
       throw std::domain_error{ "Invalid total capacity: " + std::to_string( totalCapacity ) };
     }
