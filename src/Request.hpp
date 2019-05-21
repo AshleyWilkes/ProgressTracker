1ce9c3e666428b75da18756cb478c0c7b96900d9
Ashley Wilkes <janonderek@gmail.com>
2019-05-21 06:28:45 +0200

    Fixup. Requests src formatting improved.

--- src/Request.hpp
+++ src/Request.hpp
@@ -31,7 +31,8 @@ namespace ashley::progress_tracker {
       std::size_t usedCapacity_;
   };
 
-  template<typename PS> Request<PS>::Request( std::size_t totalCapacity ) : totalCapacity_{ totalCapacity }, usedCapacity_{ 0 } {
+  template<typename PS>
+  Request<PS>::Request( std::size_t totalCapacity ) : totalCapacity_{ totalCapacity }, usedCapacity_{ 0 } {
     if ( totalCapacity < 1 ) {
       throw std::domain_error{ "Invalid total capacity: " + std::to_string( totalCapacity ) };
     }
