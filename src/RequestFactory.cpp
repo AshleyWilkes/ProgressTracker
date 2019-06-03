8412d53925fb05815d7664a92f8b2f4c59d73e39
Ashley Wilkes <janonderek@gmail.com>
2019-06-03 14:25:24 +0200

    Fixup RequestFactory interface and impl remade.

--- src/RequestFactory.cpp
+++ src/RequestFactory.cpp
@@ -1,9 +1,11 @@
 #include "RequestFactory.hpp"
+#include "RequestFactory/TimePointRequestFactory.hpp"
 
 namespace ashley::progress_tracker::request_factory {
 
   RF_Range getKnownFactoryIds() {
     static std::vector<RF_ID> knownFactoryIds {
+      "TimePointRequestFactory"
     };
     return RF_Range( knownFactoryIds );
   }
--- src/RequestFactory.hpp
+++ src/RequestFactory.hpp
@@ -1,6 +1,8 @@
 #pragma once
 #include "Request.hpp"
 
+#include "InputMap.hpp"
+
 #include "boost/program_options.hpp"
 #include "boost/range/sub_range.hpp"
 
@@ -11,35 +13,44 @@
 namespace ashley::progress_tracker::request_factory {
   using RF_ID = std::string;
   using RF_Range = boost::sub_range<std::vector<RF_ID>>;
-  using Input = boost::program_options::variables_map;
-
-  RF_Range getKnownFactoryIds();
-
-  template<typename R>
+  template<typename R, typename I>
   class RequestFactory {
     public:
-      using RF_UPtr = std::unique_ptr<RequestFactory<R>>;
+      using RF_UPtr = std::unique_ptr<RequestFactory<R, I>>;
 
-      static std::unique_ptr<R> createRequest( const Input& input, RF_ID rfId );
-
-      static RequestFactory<R>& createFactory( RF_ID rfId );
-
-      virtual std::unique_ptr<R> createRequest( const Input& input ) const = 0;
+      virtual std::unique_ptr<R> createRequest( const I& input ) const = 0;
 
       virtual ~RequestFactory() = 0;
+
+      static std::unique_ptr<RequestFactory<R, I>> createFactory( RF_ID rfId );
     private:
       static std::map<RF_ID, RF_UPtr> factoriesMap_;
   };
 
   template<typename R>
-  typename std::unique_ptr<R>
-  RequestFactory<R>::createRequest( const Input& input, RF_ID rfId ) {
-    return createFactory( rfId )->createRequest( input );
+  RF_Range getKnownFactoryIds();
+
+  template<typename R, typename I>
+  std::unique_ptr<RequestFactory<R, I>>
+  createFactory( RF_ID rfId ) {
+    auto rfIds = getKnownFactoryIds<R>();
+    if ( rfIds.find( rfId ) != rfIds.end() ) {
+      std::unique_ptr<RequestFactory<R, I>> result = RequestFactory<R, I>::createFactory( rfId );
+      if ( ! result ) {
+        throw std::invalid_argument( "RequestFactory with id " + rfId + " should have been created, but nullptr was returned instead!" );
+      }
+      return result;
+    } else {
+      throw std::invalid_argument( "RequestFactory with id " + rfId + " is not supported for the given types!" );
+    }
   }
 
-  template<typename R>
-  RequestFactory<R>&
-  RequestFactory<R>::createFactory( RF_ID rfId ) {
-    throw std::invalid_argument( "RequestFactory with id " + rfId + " is not supported!" );
+  template<typename R, typename I>
+  std::unique_ptr<R>
+  createRequest( const I& input, RF_ID rfId ) {
+    return RequestFactory<R, I>::createFactory( rfId ).createRequest( input );
   }
+
+  template<typename R, typename I>
+  RequestFactory<R, I>::~RequestFactory() = default;
 }
