3158a0f3be6a1dec3f8cdbe3990558f73dda216b
Ashley Wilkes <janonderek@gmail.com>
2019-06-03 19:44:14 +0200

    Fixup. InputMap.getAs implemetation has error diagmostic now.

--- src/InputMap.hpp
+++ src/InputMap.hpp
@@ -1,16 +1,49 @@
 #pragma once
+#include <stdexcept>
+#include "boost/any.hpp"
+
+#include <string>
 
 namespace ashley::progress_tracker::input_map {
+  template<typename T>
+  std::string
+  inline to_string( const T& val ) {
+    return std::to_string( val );
+  }
+
+  template<>
+  std::string
+  inline to_string<std::string>( const std::string& val ) {
+    return val;
+  }
+
   template<typename K, typename I>//K -- key, I -- internal value implementation
   class InputMap {
     public:
       template<typename T>
-      T getAs( const K& key ) const { return get( key ).template as<T>(); }
+      T getAs( const K& key ) const;
 
       virtual ~InputMap() = 0;
     private:
       virtual const I& get( const K& key ) const = 0;
   };
+
+  template<typename K, typename I>
+  template<typename T>
+  T
+  inline InputMap<K, I>::getAs( const K& key ) const {
+    try {
+      auto valueObject = get( key );
+      auto castValue = valueObject.template as<T>();
+      return castValue;
+    } catch ( const std::out_of_range& ) {
+      throw std::out_of_range{ "Key '" + to_string( key ) + "' not found in the input map!" };
+    } catch ( const std::invalid_argument& ) {
+      throw std::invalid_argument{ "Value of key '" + to_string( key ) + "' couldn't be converted to requested type!" };
+    } catch ( const boost::bad_any_cast& ) {
+      throw std::invalid_argument{ "Value of key '" + to_string( key ) + "' couldn't be converted to requested type!" };
+    }
+  } 
 
   template<typename K, typename I>
   InputMap<K, I>::~InputMap() = default;
